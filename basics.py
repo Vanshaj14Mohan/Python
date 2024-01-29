ddcbe4045b4571fc4a690c04c5b02f2d090b1085
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-01-30 00:00:52 +0530

    first commit

---
+++ basics.py
@@ -0,0 +1,15 @@
+#Normal print condition in python.
+print("hello world!")
+print(20+30)
+print(50-20)
+print(20*20)
+print(10/5)
+print(25%5)
+print(2**9)
+
+#Now how to use strings
+name = "Vanshaj P Mohan"
+age = 22
+email = "Vanshajkumar145@gmail.com"
+print(name, age, email)
+
