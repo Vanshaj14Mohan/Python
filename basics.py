416455dd97fd3d84ca8d23a5a0f025f2800edeca
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-01-30 00:27:22 +0530

    Data types

--- basics.py
+++ basics.py
@@ -7,9 +7,35 @@ print(10/5)
 print(25%5)
 print(2**9)
 
-#Now how to use strings
+#Now how to use variables
 name = "Vanshaj P Mohan"
 age = 22
 email = "Vanshajkumar145@gmail.com"
-print(name, age, email)
+price = 25.90
+print(name, age, email, price)
 
+#To see the type of variables:
+print(type(name))
+print(type(age))
+print(type(email))
+print(type(price))
+
+#To see datatypes
+age = 22 #Integer datatype
+amount = 88.25 #Float datatype 
+order = True #Boolean datatype
+new = None #None datatype
+
+print((type(age)), (type(amount)), (type(order)), (type(new)))
+
+#Now a small question, add two numbers
+a = 10
+b = 20
+sum = a+b
+print(sum)
+
+#same for difference
+a = 15
+b = 10
+diff = a-b
+print(diff)
