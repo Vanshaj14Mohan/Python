94673d9a6a910d1108b2f12a70e1be94833d410f
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-01-31 23:49:15 +0530

    Conditional statements

---
+++ Conditional statements/statement.py
@@ -0,0 +1,21 @@
+#if-elif-else(SYNTAX)
+
+light ="green" #It will print go now;
+
+if(light == "red"):
+    print("Stop, don't go ahead")
+elif(light == "yellow"):
+    print("Wait, look around")
+elif(light == "green"):
+    print("Go now")
+
+print("--"*10)
+
+age =22
+
+if(age >=18):
+    print("can apply for license")
+else:
+    print("cannot apply for license")
+
+print("--"*10)
