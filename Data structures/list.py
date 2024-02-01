71684e122593926972f449200fe34cd1d9ad734e
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-01 23:57:07 +0530

    Data structures

---
+++ Data structures/list.py
@@ -0,0 +1,9 @@
+#List data structure is a special data structure in python
+#Some properties of list might seem same as string
+#We can store elements of different types together(int, float, string.. etc)
+
+marks = [90.2, 95.5, 82.8, 80, 78.9, 95.1, 88.5, 92.0, 98]
+print(marks)
+print(type(marks)) #list
+print(marks[0], marks[1], marks[7], marks[5]) #90.2, 95.5, 92.0, 95.1
+print(len(marks)) #9 
