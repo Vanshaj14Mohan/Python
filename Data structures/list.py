89a16d93c094d31f6cf30d59e1a6702737605239
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-02 00:17:02 +0530

    lists

--- Data structures/list.py
+++ Data structures/list.py
@@ -7,3 +7,23 @@ print(marks)
 print(type(marks)) #list
 print(marks[0], marks[1], marks[7], marks[5]) #90.2, 95.5, 92.0, 95.1
 print(len(marks)) #9 
+
+random = ["sam", "arthur", 90, 87.25]
+print(random[0], random[1], random[2], random[3]) #sam, arthur, 90, 87.25
+print(type(random)) #list
+print(len(random)) #4
+
+#Key point:
+#1-> List are mutable in python, whereas string in python is immutable
+key =["hello", "good", "morning"]
+key[2] ="night"
+print(key[2]) #night
+print(key)# "hello", "good", "night" -> a new list
+
+#list slicing is possible here too
+print(random[0:4]) #sam, arthur, 90, 87.25
+print(random[1:3]) #arthur, 90, 87.25
+print(random[0:3]) #sam, arthur, 90
+print(random[:3]) #same as above
+print(random[-4:-1])#sam, arthur, 90
+print(random[::-1]) #print reverse order of random
