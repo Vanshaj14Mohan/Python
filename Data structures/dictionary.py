0df74e3b2354c0c85d5dc14857bdce595cd41a85
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-05 23:25:15 +0530

    Data structures

--- Data structures/dictionary.py
+++ Data structures/dictionary.py
@@ -1,43 +1,43 @@
 #DICTIONARY -> They are used to store data values in key:value pairs.
 #properties ->unordered, mutable(changeable) & dont't allow duplicate keys.
-# dict1 ={
-#     "key": "value",
-#     "name": "Vanshaj",
-#     "hobby": "coding",
-#     "age": 21,
-#     "marks": 92.5,
-#     "is_adult": True
-# }
-# print(dict1)
-# print(type(dict1)) #dict
+dict1 ={
+    "key": "value",
+    "name": "Vanshaj",
+    "hobby": "coding",
+    "age": 21,
+    "marks": 92.5,
+    "is_adult": True
+}
+print(dict1)
+print(type(dict1)) #dict
 
 #We can even store list and tuple in a dictionary. But we can't use them in place of keys only tuple coz immutable
-# subject ={
-#     "Subjects": ["Maths", "English", "Hindi", "Computer"],
-#     "Topics": ("List", "Tuple", "Dictionary", "Set")
-# }
-# print(subject)
-# print(type(subject)) #dict
+subject ={
+    "Subjects": ["Maths", "English", "Hindi", "Computer"],
+    "Topics": ("List", "Tuple", "Dictionary", "Set")
+}
+print(subject)
+print(type(subject)) #dict
 
 #to access values in dictionary
-# print(dict1["key"]) #value
-# print(dict1["name"])#Vanshaj
-# print(dict1["age"])#21
+print(dict1["key"]) #value
+print(dict1["name"])#Vanshaj
+print(dict1["age"])#21
 
 #Changing elements
-# dict1["marks"] =92.7,
-# print(dict1)
+dict1["marks"] =92.7,
+print(dict1)
 
 #Adding elements
-# dict1["skills"] = "Frontend developer",
-# print(dict1)
+dict1["skills"] = "Frontend developer",
+print(dict1)
 
 #creating a null dict and adding elements in it.
-# null_dict ={}
-# print(type(null_dict))
-# null_dict["name"] ="null"
-# null_dict["values"] ="real"
-# print(null_dict)
+null_dict ={}
+print(type(null_dict))
+null_dict["name"] ="null"
+null_dict["values"] ="real"
+print(null_dict)
 
 #Nested dictionaries
 #value -> dictionary
--- Data structures/set.py
+++ Data structures/set.py
@@ -11,8 +11,23 @@ std = set()
 print(type(std))
 
 #Methods in sets
+#1: adding elements in set
 std.add(2)
 std.add(5)
+#we can store different values in set too.
 std.add("nice")
 std.add(1)
+std.add(4)
+std.add(8)
+std.add(3)
+std.add(9)
+#we can pass tuple in it too, but we can't pass list and dict in it coz they are mutable
+std.add((6,5,2))
 print(std)
+
+#removing elements from set
+std.remove(5)
+std.remove(9)
+print(std)#will print without 5 and 9 in it.
+
+#to clear a set
