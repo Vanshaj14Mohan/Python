ad4598ddc451a5290b29214dae6727ea34ea4303
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-08 00:59:23 +0530

    Exercise

--- Data structures/Exercise4.py
+++ Data structures/Exercise4.py
@@ -1,21 +1,21 @@
 #1: Wap to ask user to enter names of their 3 favorite movies & store them in a list.
-# movie1 = input("enter movie one")
-# movie2 = input("enter movie two")
-# movie3 = input("enter movie three")
+movie1 = input("enter movie one")
+movie2 = input("enter movie two")
+movie3 = input("enter movie three")
 
-# list =[]
-# list.append(movie1)
-# list.append(movie2)
-# list.append(movie3)
-# print(list)
+list =[]
+list.append(movie1)
+list.append(movie2)
+list.append(movie3)
+print(list)
 
 # #Another way -> to directly append it
 
-# movies =[]
-# movies.append(input("enter first movie"))
-# movies.append(input("enter second movie"))
-# movies.append(input("enter third movie"))
-# print(movies)
+movies =[]
+movies.append(input("enter first movie"))
+movies.append(input("enter second movie"))
+movies.append(input("enter third movie"))
+print(movies)
 
 #2: Wap to check if a list contains a palindrome of elements.
 
