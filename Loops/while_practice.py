721bed047f4271df602cc0f77c8b09fb40a9852a
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-09 00:17:50 +0530

    Exercises

--- Loops/while_practice.py
+++ Loops/while_practice.py
@@ -21,4 +21,21 @@ while x<=10:
     x +=1
 
 #4 -> Print elements of the given list using loops
-#[1,4,9,1,25,36,49,64,81,100]
+#[1,4,9,25,36,49,64,81,100]
+nums =[1, 4, 9, 25, 36, 49, 64, 81, 100]
+idx =0
+while idx < len(nums):
+    print(nums[idx]) #nums[0], nums[1]...
+    idx += 1
+
+#5 -> Search for a number x in this tuple using loop:
+#(1,4,9,25,36,49,64,81,100)
+random = (1, 4, 9, 25, 36, 49, 64, 81, 100)
+x= 36
+i =0
+while i < len(random):
+    if(random[i] == x):
+        print("Number found at idx:", i)
+    else:
+        print("Not found")
+    i +=1
