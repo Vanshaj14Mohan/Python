72e8ef7205ab01900a2fb76e94c1d098c5137f5e
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-08 22:31:42 +0530

    Questions

---
+++ Loops/while_practice.py
@@ -0,0 +1,24 @@
+#1 -> Print numbers from 1 to 100
+i = 1
+while i<=100:
+    print(i)
+    i +=1
+print("--"*10)
+
+#2 -> Print numbers from 100 to 1
+i = 100
+while i>=1:
+    print(i)
+    i -=1
+print("--"*10)
+
+#3 -> Print the multiplication table of number n
+#Let's take n as 4
+n = int(input("enter a number"))
+x = 1
+while x<=10:
+    print(n*x)# n*x
+    x +=1
+
+#4 -> Print elements of the given list using loops
+#[1,4,9,1,25,36,49,64,81,100]
