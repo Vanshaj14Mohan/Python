49291e1c5945ed126a3479119558d2218f7f0501
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-08 22:05:52 +0530

    loops

---
+++ Loops/while.py
@@ -0,0 +1,33 @@
+#Print hello 5 times
+count = 1 #here count -> iterator,
+while count <=5:
+    print("hello")
+    count += 1
+print(count) #6
+ 
+#Print good 7 times 
+i = 1 #i -> iterator here
+while i <=7:
+    print("good")
+    i += 1
+print(i)#8
+
+#Print nice 5 times and it's value everytime
+x = 1
+while x<=5:
+    print("nice", x) #nice 1.....nice 5
+    x += 1
+print(x)#6
+
+#Print numbers from 1-10 using while loop
+i = 1
+while i<=10:
+    print(i)
+    i += 1
+
+#Print numbers from 5-1 ie. reverse form
+i = 5
+while i>=1:
+    print(i)
+    i-=1
+
