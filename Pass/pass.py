ac879505d8622f63022f66cb9fdce0b0dc862876
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-10 21:41:10 +0530

    pass statement

---
+++ Pass/pass.py
@@ -0,0 +1,10 @@
+# pass statement, generally we use pass in try and catch block or in excpetions in python.
+#pass is a null statement that does nothing. It is used as a placeholder for future code.
+
+for i in range(10):
+    pass #skip, used for place of a future code, it will directly print night now.
+print("night")
+
+if i < 5:
+    pass #will print skip directly without giving error.
+print("skip")
