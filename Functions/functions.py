70f35061171d1f6f4f293f98ce98d9c3dba876d6
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-23 20:48:56 +0530

    Functions introduction

---
+++ Functions/functions.py
@@ -0,0 +1,31 @@
+#Functions are block of statements that perform a specfic task.
+#Making function for adding two numbers.
+
+def cal_sum (a, b):
+    sum = a+b
+    print(sum)
+    return sum
+
+print("For addition")
+cal_sum(2, 5)
+print("--"*5)
+
+#For Multiplication
+def mul_fun (a, b):
+    mul = (a * b)
+    print(mul)
+    return mul
+
+print("for multiplication")
+mul_fun(2, 8)
+print("--"*5)
+
+#For substraction
+def sub_fun (a, b):
+    subs = (a - b)
+    print(subs)
+    return subs
+
+print("For substraction")
+sub_fun(40, 32)
+print("--"*5)
