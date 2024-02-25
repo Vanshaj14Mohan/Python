3e07c56b7942cdb1a035f268160ee7b20893b89c
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-02-25 20:05:56 +0530

    Recursion exercise

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -2,10 +2,37 @@
 #When a person calls itself repeatedly.
 
 #1. Print number from n to 1 backwards using recurion.
-
-def show(n):
-    if(n == 0):
+def num(n):
+    if(n == 0): #base case
         return
     print(n)
-    show(n-1)
-show(6) 
+    num(n-1)
+num(6) 
+
+#2. Print n factorial of a number
+def fac(n):
+    if(n == 0 or n ==1):
+        return 1
+    else:
+        return n * fac(n-1)
+
+print(fac(7))
+print("--"*10)
+
+#3. Write a recursive function to calculate the sum of first n natural numbers.
+def natural_no(n):
+    if(n == 0):
+        return 0
+    return natural_no(n-1) + n
+sum =natural_no(7)
+print(sum)
+
+#4. Write a recursive function to print all elements in a list. hint use list & index as paramter
+def list_no(list, idx =0):
+    if(idx ==len(list)):
+        return 
+    print(list[idx])
+    list_no(list, idx+1)
+
+languages =["Javascript", "C++", "Python", "C", "Java"]
+list_no(languages)
