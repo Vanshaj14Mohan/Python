50f1facd9acd3c6b14ae5ed9309477e51cd81ab6
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-03-19 23:07:26 +0530

    Properties in I/O files

--- Files/input_output.py
+++ Files/input_output.py
@@ -1,6 +1,11 @@
 # A file I/O syntax
 f = open("E:\Python\Files\demo.txt", "r")
-data = f.read()
-print(data)
-print(type(data))
+#data = f.read() #read entire file
+#data = f.read(7) starting 7 words of the file
+line = f.readline() #reads one line at a time
+print(line)
+line2 = f.readline()
+print(line2)
+#print(data)
+#print(type(data))
 f.close()
