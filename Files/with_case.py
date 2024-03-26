8454edd84dcc38fa148d7296d472d675163b84cb
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-03-26 22:38:56 +0530

    files

--- Files/with_case.py
+++ Files/with_case.py
@@ -4,6 +4,6 @@ with open("E:\Python\Files\with.txt", "r") as f:
 
 #f.close(), not compulsory with, with Syntax, coz with automatically closes file for us.
     
-#for "w" case:    
+#for "w" write case:    
 with open("E:\Python\Files\with.txt", "w") as f:
     f.write("New data for more test.")
