da2f57b5e9020e8092eb66cf4f252e66d7ef58c0
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-03-27 22:13:55 +0530

    Exercise

--- Files/Exercise/practice.py
+++ Files/Exercise/practice.py
@@ -6,13 +6,18 @@ newdata=data.replace("Java", "Python")
 print(newdata)
 
 with open("files/exercise/practice.txt", "w") as f:
-    f.write(newdata)
+     f.write(newdata)
 
 #to find a paticular word in the file
-word="learning"
-with open("files/exercise/practice.txt", "r") as f:
-    data=f.read()
-    if(data.find(word)!= -1):
-        print("found")
-    else:
-        print("not found")
+def check_for_words():
+        word="learning"
+        with open("files/exercise/practice.txt", "r") as f:
+            data=f.read()
+            if(data.find(word)!= -1):
+                print("found")
+            else:
+                print("not found")
+ 
+check_for_words()
+
+#to find in which line of the file does the word learning occurs first 
