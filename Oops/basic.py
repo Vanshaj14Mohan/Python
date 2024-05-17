5dc1dd6e9b6cb9639d3a29f703a270c3e80b451f
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-05-17 21:46:40 +0530

    Classes & Objects

---
+++ Oops/basic.py
@@ -0,0 +1,16 @@
+#Creating class
+class Student:
+    name="user"
+
+#Creating object(instance)
+s1 = Student()
+print(s1)
+print(s1.name)
+
+class Cars:
+    color="black"
+    brand="mercedes"
+
+car1 = Cars()
+print(car1.color)
+print(car1.brand)
