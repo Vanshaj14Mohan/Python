ccde885b7fcad58613a22482c04ce40eae6fee87
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-05-19 22:24:29 +0530

    attributes

--- Oops/attributes.py
+++ Oops/attributes.py
@@ -1,16 +1,17 @@
 #Class and Instance Attributes
 
 class Student:
+
+    college_name = "ABC College"
+    name = "anonymous" #class attribute
     
     #Parameterized constructor
-    def __init__(self, fullname, marks):  #self parameter is a reference to current instance of the class, and is used to access variables that belongs to that class.
-        self.name = fullname
+    def __init__(self, name, marks):  #self parameter is a reference to current instance of the class, and is used to access variables that belongs to that class.
+        self.name = name #obj attribute -> obj attribute > class attribute
         self.marks = marks
         print("New student added in database..")
 
 #Creating object(instance)
 s1 = Student("user", 92)
-print(s1.name, s1.marks)#same as self-> ie self and this value refers to same location
+print(s1.name)#same as self-> ie self and this value refers to same location
 
-s2 = Student("demo", 95)
-print(s2.name, s2.marks)
