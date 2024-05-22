a761d1971c46cd5c8e849f0f9af08187a2dba7f5
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-05-22 17:19:57 +0530

    Exercise

--- Oops/exercise.py
+++ Oops/exercise.py
@@ -32,7 +32,26 @@ class Account():
     def __init__(self, bal, acc):
         self.balance = bal
         self.account_no = acc
+        
+    #Debit method
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs.", amount, "was debited" )
+        print("total balance =", self.get_balance())
+
+    #Credit method
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs.", amount, "was credited" )
+        print("total balance =", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
 
 acc1 = Account(10000, 1457)
-print("Account balance" , acc1.balance)
-print("Account number" , acc1.account_no)        
+# print("Account balance" , acc1.balance)
+# print("Account number" , acc1.account_no)       
+acc1.debit(1000)
+acc1.credit(500)
+acc1.credit(50000)
+acc1.debit(5000)
