3e9941bcef9784131b2dfb211b83e3706d15dccf
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-06-02 21:51:06 +0530

    property decorator

--- Oops part 2/property.py
+++ Oops part 2/property.py
@@ -1,1 +1,11 @@
 #using property decorator
+print("property decorator")
+class Student:
+    def __init__(self,maths,cs,stats):
+        self.maths = maths
+        self.cs = cs
+        self.stats = stats
+        self.percentage = str((self.maths + self.cs + self.stats)/3) + "%"
+
+stu1 = Student(98,95,95)
+print(stu1.percentage)
