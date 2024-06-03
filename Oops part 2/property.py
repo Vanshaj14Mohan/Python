9a51528797625e25a02a8ab72e5764982c87acb4
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-06-03 23:35:27 +0530

    basic case

--- Oops part 2/property.py
+++ Oops part 2/property.py
@@ -7,5 +7,13 @@ class Student:
         self.stats = stats
         self.percentage = str((self.maths + self.cs + self.stats)/3) + "%"
 
+    def calPercentage(self):
+        self.percentage = str((self.maths + self.cs + self.stats)/3) + "%"
+
 stu1 = Student(98,95,95)
 print(stu1.percentage) #will print the average
+
+stu1.maths = 92
+print(stu1.maths)
+stu1.calPercentage()
+print(stu1.percentage)
