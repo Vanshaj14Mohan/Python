6eec8fea2d6ae49c4eb9ff2830b4cb072ac304d6
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-06-04 22:48:35 +0530

    polymorphism

---
+++ Oops part 2/polymorphism.py
@@ -0,0 +1,10 @@
+#Operator overloading
+# eg-> + operators works differentely with different datatypes
+print(1+2)#3
+print(type(1))# shows class int
+
+print("good" + "day")#concatenate
+print(type("good"))# shows class string
+
+print([1,2,3] + [4,5,6])#merge
+print(type([1,2,3])) #shows class list
