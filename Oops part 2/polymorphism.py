a7bd7921bd0f485f98494272b3a5822969945b00
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-06-05 19:18:58 +0530

    complex class

--- Oops part 2/polymorphism.py
+++ Oops part 2/polymorphism.py
@@ -8,4 +8,21 @@ print(type("good"))# shows class string
 
 print([1,2,3] + [4,5,6])#merge
 print(type([1,2,3])) #shows class list
-#So basically same operator is allowed to have different meaning acc to context..
+#So basically same operator is allowed to have different meaning acc to context.. 
+
+#Creating a complex class
+print("Complex class")
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i +", self.img, "j")
+
+#creating a complex number
+num1 = Complex(2,5)
+num1.showNumber()
+
+num2 = Complex(5,7)
+num2.showNumber()
