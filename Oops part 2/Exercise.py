2313a72ca414c077e00b9b2a2bb5c9e337445e2e
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-06-09 23:00:49 +0530

    Exercises

---
+++ Oops part 2/Exercise.py
@@ -0,0 +1,16 @@
+#Q 1-> Define a circle class with radius r using the constructor. Define an Area() method of the class 
+# which calculates the area of the circle. Define a Perimeter() method of the class which allows you to 
+# calculate the perimeter of the circle.
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return (22/7) * self.radius **2
+    
+    def perimeter(self):
+        return (22/7) * 3.14 *self.radius
+    
+c1 = Circle(21)
+print(c1.area())
+print(c1.perimeter())
